7346d8d0f70b27357ea237bc8670be7a466b3243
Yousuf Azad (Sami) <sami1592@gmail.com>
2018-08-14 18:30:40 +0200

    Networking Player Health

--- Assets/Health.cs
+++ Assets/Health.cs
@@ -1,23 +1,33 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Networking;
 using System.Collections;
 
-public class Health : MonoBehaviour
+public class Health : NetworkBehaviour
 {
 
     public const int maxHealth = 100;
+
+    [SyncVar(hook = "OnChangeHealth")]
     public int currentHealth = maxHealth;
+
     public RectTransform healthBar;
 
     public void TakeDamage(int amount)
     {
+        if (!isServer)
+            return;
+
         currentHealth -= amount;
         if (currentHealth <= 0)
         {
             currentHealth = 0;
             Debug.Log("Dead!");
         }
+    }
 
-        healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
+    void OnChangeHealth(int health)
+    {
+        healthBar.sizeDelta = new Vector2(health, healthBar.sizeDelta.y);
     }
 }
