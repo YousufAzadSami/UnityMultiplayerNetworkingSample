f79a00712db5a8489f5409aa2d5f1811d80a42bd
Yousuf Azad (Sami) <sami1592@gmail.com>
2018-08-14 18:02:10 +0200

    Player Health (Single Player) PS: There are probably mistakes

---
+++ Assets/Health.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Health : MonoBehaviour
+{
+
+    public const int maxHealth = 100;
+    public int currentHealth = maxHealth;
+    public RectTransform healthBar;
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            Debug.Log("Dead!");
+        }
+
+        healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
+    }
+}
