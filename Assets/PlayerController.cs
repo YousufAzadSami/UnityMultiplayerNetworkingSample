dfadd2fc5c8cc15bc58cf0c9933816b334a6d7c3
Yousuf Azad (Sami) <sami1592@gmail.com>
2018-08-14 15:58:49 +0200

    Testing Multiplayer Movement

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,9 +1,13 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : NetworkBehaviour
 {
     void Update()
     {
+        if (!isLocalPlayer)
+            return;
+
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
         var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
 
